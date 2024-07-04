308544d222a70a128ec2e91a0221a5530e638d9d
flheight <75139846+flheight@users.noreply.github.com>
2024-07-04 19:54:01 +0200

    Update make_mnist_umap.py

--- make_mnist_umap.py
+++ make_mnist_umap.py
@@ -77,7 +77,7 @@ def create_scatter_plot(df, show_legend):
             name=label
         ))
     fig.update_layout(
-        title='',
+        plot_bgcolor='white',
         showlegend=show_legend,
         legend_title_text='Label',
         legend_traceorder='normal',
